4f142fbacfdcaae7ec90c74ca8aca9845c7f7608
Ahlem Said <ahlemsaid908@gmail.com>
2022-09-17 07:49:37 -0700

    Add the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import webImages from "../images/web.png";
 
 function home() {
   return (
-    <div>home</div>
+    <div>
+     
+     <img src="/images/my-photo.jpg" alt="my photo" className='avatar' />
+     <h1>Hello My name is Ahlem. I am a Fullstack Developper.</h1>
+     <img src={webImages} alt="web Development" />
+    </div>
   )
 }
 
